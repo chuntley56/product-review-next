3706efb1f3e9c62efb97c6acbc640b5d32fbfad4
chuntley56 <coreyhuntley562@gmail.com>
2019-02-27 16:22:18 -0800

    change url in env variable

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withFonts = require('next-fonts');
 module.exports =
 withCSS(withFonts({
   enableSvg: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/product-review-next' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://chuntley56.github.io/product-review-next/' : '',
   exportPathMap: function () {
     return {
       '/': { page: '/' }
